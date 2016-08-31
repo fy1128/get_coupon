d161da7944c7ec3cde696e9e83d2649428793cf1
fy1128 <l.fuying@yahoo.com>
2016-08-31 16:19:53 +0800

    Get server time with the url's host; And print the comment of the url when print; Use try except to catch error from get_page().

--- get_coupon.py
+++ get_coupon.py
@@ -4,24 +4,41 @@ import requests
 import time,datetime,threading,httplib
 import re
 from multiprocessing.dummy import Pool as ThreadPool
+from urlparse import urlparse
 
-def get_userdata(file_url):
+def get_userdata(file_url, dlist = False):
     data=[]
     with open(file_url,'r') as f1:
         flists=f1.readlines()
+        if dlist:
+            globals()[dlist] = flists
         for flist in flists:
             if flist[-1]=='\n':
                 flist=flist[0:-1]
-            data.append(flist)
+            if flist.strip() and (flist[0] != '#'):
+                data.append(flist)
         data=tuple(data)
         return data
 
-urls=get_userdata('urls.txt')
+urls=get_userdata('urls.txt', 'urls_parent')
 cookies=get_userdata('cookies.txt')
 s=requests.session()
-x=int(input('\n请选择第x个url：'))
 
-def get_webservertime(host='www.jd.com'):
+i = 0
+for l in urls:
+    lindex = urls_parent.index(l+"\n")
+    lpre = urls_parent[lindex-1].strip()
+    comment = ''
+    if lpre[0] == '#':
+        comment = lpre[1:].strip() + ' -> '
+    i += 1
+    print(str(i)+') ' + comment + l)
+
+x=int(input('\n请选择第x个url：'))
+url = urls[x-1]
+url_params = urlparse(url)
+
+def get_webservertime(host = url_params.hostname):
     conn=httplib.HTTPConnection(host)
     conn.request("GET", "/")
     r=conn.getresponse()
@@ -29,8 +46,8 @@ def get_webservertime(host='www.jd.com'):
     ts=  r.getheader('date') #获取http头date部分
     #将GMT时间转换成北京时间
     ltime= time.strptime(ts[5:25], "%d %b %Y %H:%M:%S")
-    print('\n时间校对：'+str(datetime.datetime.now().strftime('%H:%M:%S'))+' / '+str(ts[5:25]))
     nowtime=time.localtime(time.mktime(ltime)+8*60*60)
+    print('\n时间校对('+url_params.hostname+'): '+str(datetime.datetime.now().strftime('%H:%M:%S'))+' / '+str(nowtime[3])+':'+str(nowtime[4])+':'+str(nowtime[5]))
     starttime=time.strptime(str(nowtime[0])+'-'+str(nowtime[1])+'-'+str(nowtime[2])+' '+str(target_time),'%Y-%m-%d %H:%M:%S')
     starttime=datetime.datetime(starttime[0],starttime[1],starttime[2],starttime[3],starttime[4],starttime[5])
     nowtime=datetime.datetime(nowtime[0],nowtime[1],nowtime[2],nowtime[3],nowtime[4],nowtime[5])
@@ -43,22 +60,26 @@ def get_page(cookie):
     'Accept-Language':'zh-CN,zh;q=0.8',
     'Connection':'keep-alive',
     'Cookie':cookie,
-    'Host':'coupon.jd.com',
+    'Host':url_params.hostname,
     'Cache-Control':'max-age=0',
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
     }
-    url=urls[x-1]
     for i in range(0,3,1):
-        r=s.get(url,headers=headers,timeout=60)
-    #    print(r.text)
-        cer=re.compile('<h1 class="ctxt02"><s class="icon-redbag"></s>(.*)</h1>',flags=0)
-        strlist=cer.findall(r.text)
-        if not strlist:
-            print('未知错误')
-        else:
-            print(strlist[0])
-        time.sleep(3)
+        try:
+            r=s.get(url,headers=headers,timeout=60)
+        #    print(r.text)
+            cer=re.compile('<h1 class="ctxt02"><s class="icon-redbag"></s>(.*)</h1>',flags=0)
+            strlist=cer.findall(r.text)
+            if not strlist:
+                print(datetime.datetime.now().strftime('%H:%M:%S') +': ' + '未知错误')
+            else:
+                print(datetime.datetime.now().strftime('%H:%M:%S') +': ' + strlist[0])
+        except Exception as e:
+            print(datetime.datetime.now().strftime('%H:%M:%S') + ': ' + str(e))
+            continue
+
+        time.sleep(1)        
 
 #模式1：对单个用户进行get操作
 def one_get():
